508e339d2498bc2b0493033a87775f264e39399a
LAPTOP-LF28MDJ2\yassi <LAARIBI_LAHLOUYASSINE@emsi-edu.ma>
2022-11-19 19:07:33 +0100

    FC

---
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,27 @@
+package org.sid.customerservice;
+
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository){
+        return args -> {
+            customerRepository.save(new Customer(null,"mouad","med@gmail.com"));
+            customerRepository.save(new Customer(null,"Yassine","hassan@gmail.com"));
+            customerRepository.save(new Customer(null,"Salima","salima@gmail.com"));
+            customerRepository.findAll().forEach(c->{
+                System.out.println(c.toString());
+            });
+        };
+    }
+}
---
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,29 @@
+package org.sid.inventoryservice;
+
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository){
+        return args -> {
+            productRepository.save(new Product(null,"Ordinateur",788,12));
+            productRepository.save(new Product(null,"Imprimante",88,129));
+            productRepository.save(new Product(null,"Smartphone",1288,112));
+            productRepository.findAll().forEach(p->{
+                System.out.println(p.getName());
+            });
+        };
+    }
+}
+
+
